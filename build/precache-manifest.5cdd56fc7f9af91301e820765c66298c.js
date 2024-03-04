84cb95663533ce611c992514ade3013e2721c825
Rosheen Chaudhry <rosheenc123@gmail.com>
2024-03-03 23:32:12 -0500

    init

---
+++ build/precache-manifest.5cdd56fc7f9af91301e820765c66298c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192ef571eb8fe0afcd2bbfa3873edc7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65fd8246430ff0e5507b",
+    "url": "/static/css/main.0e734603.chunk.css"
+  },
+  {
+    "revision": "67b9689ae2484f17ba70",
+    "url": "/static/js/2.e07e5126.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.e07e5126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65fd8246430ff0e5507b",
+    "url": "/static/js/main.a2b4ce1e.chunk.js"
+  },
+  {
+    "revision": "9fcb7f224234706e8579",
+    "url": "/static/js/runtime-main.685bbc60.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/static/media/logo.be622899.svg"
+  }
+]);
